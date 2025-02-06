956a1c91c7b4dbdfee44433b3b9437a6002a647d
Frank B. <cfrankb.2000@gmail.com>
2025-02-05 22:59:56 -0500

    tile display test

--- apps/cs3_vga/src/main.cpp
+++ apps/cs3_vga/src/main.cpp
@@ -1,17 +1,28 @@
+#include <stdio.h>
+#include <time.h>
 #include "gvga.h"
 #include "gfx.h"
 #include "pico/stdlib.h"
-#include <stdio.h>
 #include "ls7447.h"
 #include "data.h"
+#include "tilesdata.h"
 
 /**
  * simple hello world program demonstrating the GVga library
  */
 
+#define TILE_SIZE 16
+#define TILES_MAX TILES_LEFTBIRD
+
 const uint BOARD_LED_PIN = 25; // Example: GPIO 25, which is connected to the onboard LED
 int _state = 1;
 uint32_t _msLast;
+
+const int width = 320;			 // screen width
+const int height = 240;			 // screen height
+const int bits = 8;				 // bits per pixel (256 color palette)
+const bool doubleBuffer = false; // not enough memory for that
+const bool interlaced = false;
 
 static void _init_led()
 {
@@ -57,18 +68,41 @@ size_t setPalette(GVga *gvga)
 	return colorCount;
 }
 
+void drawTile(GVga *gvga, int baseX, int baseY, uint16_t tileId)
+{
+	uint8_t *tile = tiles_mcz + tileId * 256;
+	auto ptr = gvga->drawFrame + gvga->rowBytes * baseY + baseX;
+	for (int y = 0; y < 16; ++y)
+	{
+		for (int x = 0; x < 16; ++x)
+		{
+			ptr[x] = tile[x];
+		}
+		tile += 16;
+		ptr += gvga->rowBytes;
+	}
+}
+
+void fillScreen(GVga *gvga)
+{
+	const int cols = width / TILE_SIZE;
+	const int rows = height / TILE_SIZE;
+	for (int y = 0; y < rows; ++y)
+	{
+		for (int x = 0; x < cols; ++x)
+		{
+			const int tileID = rand() % TILES_MAX;
+			drawTile(gvga, x * TILE_SIZE, y * TILE_SIZE, tileID);
+		}
+	}
+}
+
 int main()
 {
 	stdio_init_all();
+	srand(time(NULL));
 	sleep_ms(1000);
 	printf("\nGVga test\n");
-
-	int width = 320;  // 640;
-	int height = 240; // 480;
-	int bits = 8;	  // 1;
-	bool doubleBuffer = false;
-	bool interlaced = false;
-	// bool sync = false;
 
 	for (int i = 15; i; --i)
 	{
@@ -76,10 +110,9 @@ int main()
 		printf("loop %d\n", i);
 		sleep_ms(250);
 	}
-
 	_init_led();
 
-	GVga *gvga = gvga_init(width, height, bits, doubleBuffer, interlaced, NULL); // note: double-buffering enabled
+	GVga *gvga = gvga_init(width, height, bits, doubleBuffer, interlaced, NULL);
 	gvga_start(gvga);
 	auto colorCount = setPalette(gvga);
 
@@ -87,10 +120,8 @@ int main()
 	while (true)
 	{
 		_blink_led(100);
-		gfx_clear(gvga, i);
-		// if (sync)
-		//	gvga_sync(gvga);	// wait for the other core to finish displaying the frame buffer
-		// gvga_swap(gvga, false); // double-buffering with/without copy
+		// gfx_clear(gvga, i);
+		fillScreen(gvga);
 		sleep_ms(1000);
 		i++;
 		if (i >= colorCount)
